12b6ba901cdf9c02cf573bd0d279894f0221d75a
neniya222 <neniya222@gmail.com>
2026-03-17 16:17:03 +0900

    Wikiの名前を正式名称に変更しました。

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'MLS Wiki',
+			title: 'MiLight Server Wiki',
 
 			customCss: [
 				'./src/custom.css',
